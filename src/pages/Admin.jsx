04e9b937fdfc8e874ad16ba23f1bb862121aebaf
Tolegen777 <tolegenmukan@gmail.com>
2023-04-02 12:31:11 +0600

    Add Loyout of all pages

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -81,6 +81,7 @@ const Admin = () => {
                 onSelectRowCount={setSelectedRowCount}
                 onSelectCurrentPage={setCurrentPage}
                 onOpenCreateUpdateModal={onOpenCreateUpdateModal}
+
             />
         </>
     );
